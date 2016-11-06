0160aec058c718afe04701a3cf197882241f4445
Jean Hominal <jhominal@gmail.com>
2016-11-06 18:53:19 +0100

    Remove '=None' in Parameter Info tooltip when default is not set in the decorator

--- src/com/wishtack/pysynthetic/psi/SyntheticInitParameter.java
+++ src/com/wishtack/pysynthetic/psi/SyntheticInitParameter.java
@@ -1,11 +1,9 @@
 package com.wishtack.pysynthetic.psi;
 
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.jetbrains.python.PyElementTypes;
 import com.jetbrains.python.psi.PyExpression;
 import com.jetbrains.python.psi.impl.PyNamedParameterImpl;
-import com.jetbrains.python.psi.impl.PyNoneLiteralExpressionImpl;
 import com.jetbrains.python.psi.impl.stubs.PyNamedParameterStubImpl;
 import com.jetbrains.python.psi.stubs.PyNamedParameterStub;
 import com.jetbrains.python.psi.types.PyType;
@@ -18,9 +16,6 @@ import org.jetbrains.annotations.Nullable;
  * Created by Jean Hominal on 2016-11-06.
  */
 public class SyntheticInitParameter extends PyNamedParameterImpl {
-
-    private static final PyExpression NONE_MINIMAL_EXPRESSION =
-            new PyNoneLiteralExpressionImpl(new LeafPsiElement(PyElementTypes.NONE_LITERAL_EXPRESSION, "None"));
 
     @NotNull
     private static PyNamedParameterStub createVirtualStub(@NotNull SyntheticMemberInfo syntheticMemberInfo) {
@@ -43,8 +38,7 @@ public class SyntheticInitParameter extends PyNamedParameterImpl {
     @Nullable
     @Override
     public PyExpression getDefaultValue() {
-        PyExpression defaultValue = myMemberInfo.getDefaultValue();
-        return defaultValue != null ? defaultValue : NONE_MINIMAL_EXPRESSION;
+        return myMemberInfo.getDefaultValue();
     }
 
     @Override
